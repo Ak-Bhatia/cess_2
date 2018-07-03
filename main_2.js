3fb3c567ee0e77bd0fdb255a8ea13121f147193c
Ak-Bhatia <33599912+Ak-Bhatia@users.noreply.github.com>
2018-07-03 20:19:50 +0530

    Update main_2.js

--- main_2.js
+++ main_2.js
@@ -13,28 +13,28 @@ var txt_4 = document.querySelector('#txt_4');
 
 cont_1.addEventListener("mouseover",function(){
 	this.classList.add("hov_cont");
-	cont_1h.classList.add("col-lg-4","col-md-8","border","border-danger");
+	cont_1h.classList.add("col-lg-6","col-md-6","border","border-danger");
 	txt_1.classList.add("p-4");
 	txt_1.innerHTML = "We focus on Skill development,we organise sessions,flash classes for students that help in enhancing their skills.";
 });
 
 cont_1.addEventListener("mouseout",function(){
 	this.classList.remove("hov_cont");
-	cont_1h.classList.remove("col-lg-4","col-md-8","border","border-danger");
+	cont_1h.classList.remove("col-lg-6","col-md-6","border","border-danger");
 	txt_1.classList.remove("p-4");
 	txt_1.innerHTML = "";
 });
 
 cont_2.addEventListener("mouseover",function(){
 	this.classList.add("hov_cont");
-	cont_2h.classList.add("col-lg-4","col-md-8","border","border-danger");
+	cont_2h.classList.add("col-lg-6","col-md-6","border","border-danger");
 	txt_2.classList.add("p-4");
 	txt_2.innerHTML = "We work in teams for successful fulfillation of various jobs,we here at CESS have Technical, Literary, Placement, Promotion, Web Dev Teams.";
 });
 
 cont_2.addEventListener("mouseout",function(){
 	this.classList.remove("hov_cont");
-	cont_2h.classList.remove("col-lg-4","col-md-8","border","border-danger");
+	cont_2h.classList.remove("col-lg-6","col-md-6","border","border-danger");
 	txt_2.classList.remove("p-4");
 	txt_2.innerHTML = "";
 });
@@ -42,14 +42,14 @@ cont_2.addEventListener("mouseout",function(){
 
 cont_3.addEventListener("mouseover",function(){
 	this.classList.add("hov_cont");
-	cont_3h.classList.add("col-lg-4","col-md-8","border","border-danger");
+	cont_3h.classList.add("col-lg-6","col-md-6","border","border-danger");
 	txt_3.classList.add("p-4");
 	txt_3.innerHTML = "We have over 300 members in CESS.All the volunteers and members are categorized into various teams.";
 });
 
 cont_3.addEventListener("mouseout",function(){
 	this.classList.remove("hov_cont");
-	cont_3h.classList.remove("col-lg-4","col-md-8","border","border-danger");
+	cont_3h.classList.remove("col-lg-6","col-md-6","border","border-danger");
 	txt_3.classList.remove("p-4");
 	txt_3.innerHTML = "";
 });
@@ -57,14 +57,16 @@ cont_3.addEventListener("mouseout",function(){
 
 cont_4.addEventListener("mouseover",function(){
 	this.classList.add("hov_cont");
-	cont_4h.classList.add("col-lg-4","col-md-8","border","border-danger");
+	cont_4h.classList.add("col-lg-6","col-md-6","border","border-danger");
 	txt_4.classList.add("p-4");
 	txt_4.innerHTML = "We undertake projects and we have gone quite a far in terms of achievements.";
 });
 
 cont_4.addEventListener("mouseout",function(){
 	this.classList.remove("hov_cont");
-	cont_4h.classList.remove("col-lg-4","col-md-8","border","border-danger");
+	cont_4h.classList.remove("col-lg-6","col-md-6","border","border-danger");
 	txt_4.classList.remove("p-4");
 	txt_4.innerHTML = "";
 });
+
+
